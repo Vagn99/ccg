752dd8f91afd39c50bb48b52223f0043c9b400c2
Vagn99 <christoffer.lorenzen@outlook.com>
2024-05-12 12:23:30 +0200

    Misc

--- ignoreFile.go
+++ ignoreFile.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func readIgnoreFile(path string) ([]string, error) {
+	ignoreFilePath := filepath.Join(path, ".ccgignore")
+	file, err := os.Open(ignoreFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// If the file does not exist, return an empty list (no ignores)
+			return nil, nil
+		}
+		// For other types of errors, return the error to be handled upstream
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" && !strings.HasPrefix(line, "#") { // Ignore empty lines and comments
+			lines = append(lines, line)
+		}
+	}
+	return lines, scanner.Err()
+}
+
+func ignoreFile(fileName string, ignoreList []string) bool {
+	for _, ignore := range ignoreList {
+		if fileName == ignore {
+			return true
+		}
+	}
+	return false
+}
--- proccesDirectory.go
+++ proccesDirectory.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -38,37 +37,4 @@ func processDirectory(path string, recursive bool, result *strings.Builder) {
 			}
 		}
 	}
-}
-
-func readIgnoreFile(path string) ([]string, error) {
-	ignoreFilePath := filepath.Join(path, ".ccgignore")
-	file, err := os.Open(ignoreFilePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// If the file does not exist, return an empty list (no ignores)
-			return nil, nil
-		}
-		// For other types of errors, return the error to be handled upstream
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line != "" && !strings.HasPrefix(line, "#") { // Ignore empty lines and comments
-			lines = append(lines, line)
-		}
-	}
-	return lines, scanner.Err()
-}
-
-func ignoreFile(fileName string, ignoreList []string) bool {
-	for _, ignore := range ignoreList {
-		if fileName == ignore {
-			return true
-		}
-	}
-	return false
 }
