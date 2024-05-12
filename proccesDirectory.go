74e7d4df43fc879d16bdd837b3c1057bb5a98a3f
Vagn99 <christoffer.lorenzen@outlook.com>
2024-05-12 11:48:41 +0200

    Yup

--- proccesDirectory.go
+++ proccesDirectory.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 func processDirectory(path string, recursive bool) {
-	files, err := ioutil.ReadDir(path)
+	files, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Println("Error reading directory:", err)
 		return
@@ -17,7 +17,7 @@ func processDirectory(path string, recursive bool) {
 	var result strings.Builder
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
-		if file.Name() == "ccg" {
+		if ignoreFile(file.Name()) {
 			continue
 		}
 		if file.IsDir() {
@@ -25,14 +25,32 @@ func processDirectory(path string, recursive bool) {
 				processDirectory(filePath, recursive)
 			}
 		} else {
-			fileContents, err := ioutil.ReadFile(filePath)
+			//Include filepath and name in the result and a divider line
+			result.WriteString(filePath)
+			result.WriteString(strings.Repeat("-", len(filePath)) + "\n")
+			fileContents, err := os.ReadFile(filePath)
 			if err == nil {
 				result.WriteString(string(fileContents) + "\n")
 			}
 		}
 	}
 
-	// Save or handle the concatenated result
-	fmt.Println(result.String())
+	// Print the result to context.txt
+	contextFilePath := filepath.Join(path, "context.txt")
+	err = os.WriteFile(contextFilePath, []byte(result.String()), 0644)
+	if err != nil {
+		fmt.Println("Error writing context file:", err)
+	}
 
+}
+
+func ignoreFile(fileName string) bool {
+	//List of excluded filenames
+	ignoreList := []string{"gcc", "context.txt"}
+	for _, ignore := range ignoreList {
+		if fileName == ignore {
+			return true
+		}
+	}
+	return false
 }
